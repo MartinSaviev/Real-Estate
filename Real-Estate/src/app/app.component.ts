b0c511da6ac3bd3709010ce72a05150b477a5151
Martin Saviev <m.saviev@abv.bg>
2024-11-10 19:33:27 +0200

    create last-three-estate components

--- Real-Estate/src/app/app.component.ts
+++ Real-Estate/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LastThreeEstateComponent } from './components/last-three-estate/last-three-estate.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomePageComponent,NavbarComponent],
+  imports: [RouterOutlet,HomePageComponent,NavbarComponent,LastThreeEstateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Real-Estate/src/app/components/last-three-estate/last-three-estate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-last-three-estate',
+  standalone: true,
+  imports: [],
+  templateUrl: './last-three-estate.component.html',
+  styleUrl: './last-three-estate.component.css'
+})
+export class LastThreeEstateComponent {
+
+}
