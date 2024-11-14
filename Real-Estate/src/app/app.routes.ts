ecc8bd745bfca690ffe0606a54f573e1b40b05a5
Martin Saviev <m.saviev@abv.bg>
2024-11-14 22:10:19 +0200

    add new component add-new-real-estate

--- Real-Estate/src/app/app.routes.ts
+++ Real-Estate/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { AddNewRealEstateComponent } from './components/add-new-real-estate/add-new-real-estate.component';
 
 export const routes: Routes = [
     {path: "", component:HomePageComponent},
     {path: "register", component: RegisterComponent},
-    {path: "login", component:LoginComponent}
+    {path: "login", component:LoginComponent},
+    {path: "add-new-real-estate", component: AddNewRealEstateComponent}
 ];
---
+++ Real-Estate/src/app/components/add-new-real-estate/add-new-real-estate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-real-estate',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-new-real-estate.component.html',
+  styleUrl: './add-new-real-estate.component.css'
+})
+export class AddNewRealEstateComponent {
+
+}
